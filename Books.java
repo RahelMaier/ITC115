f18d9410c047c8da6af3e2304f990ff547730ad3
RMG8 <56052470+RahelMaier@users.noreply.github.com>
2020-03-23 23:45:34 -0700

    Update Books.java

--- Books.java
+++ Books.java
@@ -1,9 +1,273 @@
 
-public class Books {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+public class Book {
+
+    
+    private String title;
+
+    
+    private String author;
+
+
+    private double price;
+
+
+
+  
+
+    public String getTitle() {
+
+        return this.title;
+
+    }
+
+
+
+   
+    public void setTitle(String title) {
+
+        this.title = title;
+
+    }
+
+
+
+
+    public String getAuthor() {
+
+        return this.author;
+
+    }
+
+
+
+
+    public void setAuthor(String author) {
+
+        this.author = author;
+
+    }
+
+
+
+  
+
+    public void setPrice(double price) {
+
+        if (price > 0) {
+
+            this.price = price;
+
+        } else {
+
+            System.out.println("Invalid price");
+
+        }
+
+    }
+
+
+
+   
+
+    public double getPrice() {
+
+        return this.price;
+
+    }
+
+
+
+    public void sell(int qty) {
+
+        System.out.println("Total: $" + (qty * price));
+
+    }
+
+}
+
+
+
+public class Magazine {
+
+   
+
+    private String title;
+
+   
+    private String issue;
+
+   
+
+    private double price;
+
+
+
+   
+
+    public String getTitle() {
+
+        return this.title;
+
+    }
+
+
+
+   
+
+    public void setTitle(String title) {
+
+        this.title = title;
+
+    }
+
+
+
+   
+
+    public String getIssue() {
+
+        return this.issue;
+
+    }
+
+
+
+   
+
+    public void setIssue(String issue) {
+
+        this.issue = issue;
+
+    }
+
+
+
+    
+
+    public void setPrice(double price) {
+
+        if (price > 0) {
+
+            this.price = price;
+
+        } else {
+
+            System.out.println("Invalid price");
+
+        }
+
+    }
+
+
+
+    public double getPrice() {
+
+        return this.price;
+
+    }
+
+
+
+    public void sell(int qty) {
+
+        System.out.println("Total: $" + (qty * price));
+
+    }
+
+}
+
+
+
+
+
+
+public class video DVDs and CDs {
+
+	   
+
+    private String title;
+
+   
+    private String issue;
+
+   
+
+    private double price;
+
+
+
+   
+
+    public String getTitle() {
+
+        return this.title;
+
+    }
+
+
+
+   
+
+    public void setTitle(String title) {
+
+        this.title = title;
+
+    }
+
+
+
+   
+
+    public String getIssue() {
+
+        return this.issue;
+
+    }
+
+
+
+   
+
+    public void setIssue(String issue) {
+
+        this.issue = issue;
+
+    }
+
+
+
+    
+
+    public void setPrice(double price) {
+
+        if (price > 0) {
+
+            this.price = price;
+
+        } else {
+
+            System.out.println("Invalid price");
+
+        }
+
+    }
+
+
+
+    public double getPrice() {
+
+        return this.price;
+
+    }
+
+
+
+    public void sell(int qty) {
+
+        System.out.println("Total: $" + (qty * price));
+
+    }
 
 }
